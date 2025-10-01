91ab1b02111e408c11fc892a7865d7f997f8ba01
BraxtonBrown <10944268@uvu.edu>
2025-10-01 11:57:42 -0600

    mod cap doc string and math.atan2 funcation

--- Mo-Cap/Mo_Cap_Main.py
+++ Mo-Cap/Mo_Cap_Main.py
@@ -2,4 +2,15 @@ import maya.cmds as cmds
 
 def mo_cap_main():
     cmds.defineDataServer(server = "127.0.0.1:5052", device = "moCap")
-    cmds.listInputDeviceAxes("handTracker")
+    cmds.listInputDeviceAxes("moCap")
+
+    # cmds.defineDataServer(undefine=True, d="handTracker") # undefine the device if needed use this to stop the data stream and end the program
+    
+    """
+    listDeviceAttachments 
+    listInputDeviceAxes take the current imported device and lists all the axes that are available to be connected to attributes in Maya.
+    
+    need to assign the axes to the attributes in Maya
+    
+    """
+    
