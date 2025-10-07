7e51a5eb8fd25d864f1630bf21a214bd387377ff
BraxtonBrown <10944268@uvu.edu>
2025-10-06 21:44:21 -0600

    script check

--- Mo-Cap/Mo_Cap_Main.py
+++ Mo-Cap/Mo_Cap_Main.py
@@ -3,7 +3,6 @@ import maya.cmds as cmds
 def mo_cap_main():
     cmds.defineDataServer(server = "127.0.0.1:5052", device = "moCap")
     cmds.listInputDeviceAxes("moCap")
-
     # cmds.defineDataServer(undefine=True, d="handTracker") # undefine the device if needed use this to stop the data stream and end the program
     
     """
@@ -11,6 +10,8 @@ def mo_cap_main():
     listInputDeviceAxes take the current imported device and lists all the axes that are available to be connected to attributes in Maya.
     
     need to assign the axes to the attributes in Maya
+    
+    check blue tooth camera and input reevaluation
     
     """
     
